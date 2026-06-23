25e7cfd978bb3a63edd22d041be8cc6f2d86cd26
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-06-23 16:58:27 +0200

    feat: add header with siecs logo

--- src/features/Root.tsx
+++ src/features/Root.tsx
@@ -1,5 +1,19 @@
+import { Flex, Image } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
+function Header() {
+  return (
+    <Flex p={4}>
+      <Image src="/logo.png" minW={"30px"} h={"30px"} />
+    </Flex>
+  );
+}
+
 export default function Root() {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
