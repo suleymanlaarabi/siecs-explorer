976286f56b76f9e6a51bedb610bd76910ceb41e6
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-06-24 22:00:24 +0200

    feat: improve entity tree style

---
+++ src/features/world/EntityTree.tsx
@@ -0,0 +1,128 @@
+import { TreeView, createTreeCollection } from "@chakra-ui/react";
+import { Box, ChevronDown, ChevronRight } from "lucide-react";
+import { useEntities } from "../../hooks/useEntities";
+import { useMemo, useState } from "react";
+import { siecsClient, type Entity } from "../../client";
+import { useQueryClient } from "@tanstack/react-query";
+
+type EntityNode = Entity & {
+  id: string;
+  children?: EntityNode[];
+  childrenCount?: number;
+};
+
+function toEntityNode(entity: Entity): EntityNode {
+  return {
+    ...entity,
+    id: `${entity.index}:${entity.generation}`,
+    childrenCount: entity.hasChildren ? 1 : 0,
+  };
+}
+
+function withLoadedChildren(
+  loadedChildren: Record<string, EntityNode[]>,
+  node: EntityNode,
+): EntityNode {
+  return {
+    ...node,
+    children: loadedChildren[node.id]?.map((node) =>
+      withLoadedChildren(loadedChildren, node),
+    ),
+  };
+}
+
+export function EntityTree() {
+  const queryClient = useQueryClient();
+  const { data: entities = [] } = useEntities();
+
+  const [loadedChildren, setLoadedChildren] = useState<
+    Record<string, EntityNode[]>
+  >({});
+
+  const collection = useMemo(() => {
+    const root = {
+      id: "root",
+      name: "root",
+      index: -1,
+      generation: 0,
+      hasChildren: true,
+      children: entities
+        .map(toEntityNode)
+        .map((node) => withLoadedChildren(loadedChildren, node)),
+    } satisfies EntityNode;
+
+    return createTreeCollection<EntityNode>({
+      rootNode: root,
+      nodeToValue: (node) => node.id,
+      nodeToString: (node) => node.name,
+      nodeToChildren: (node) => node.children || [],
+    });
+  }, [entities, loadedChildren]);
+
+  return (
+    <TreeView.Root
+      collection={collection}
+      rounded="none"
+      height="100%"
+      bg={"bg.panel"}
+      p={1}
+      lazyMount
+      loadChildren={async ({ node }) => {
+        const children = await queryClient.fetchQuery({
+          queryKey: ["entities", node.index, node.generation, "children"],
+          queryFn: async () => {
+            const entities = await siecsClient.entities(node);
+            return entities.map(toEntityNode);
+          },
+        });
+
+        setLoadedChildren((current) => ({
+          ...current,
+          [node.id]: children,
+        }));
+
+        return children;
+      }}
+    >
+      <TreeView.Tree gap={1}>
+        <TreeView.Node
+          indentGuide={<TreeView.BranchIndentGuide />}
+          render={({ node, nodeState }) =>
+            node.hasChildren ? (
+              <TreeView.BranchControl
+                my={1}
+                rounded="xs"
+                _hover={{
+                  bg: "bg.emphasized/60",
+                }}
+                _selected={{
+                  bg: "bg.muted",
+                }}
+              >
+                {nodeState.expanded ? <ChevronDown /> : <ChevronRight />}
+
+                <TreeView.BranchText fontSize="md">
+                  {node.name}
+                </TreeView.BranchText>
+              </TreeView.BranchControl>
+            ) : (
+              <TreeView.Item
+                rounded="xs"
+                _hover={{
+                  bg: "bg.emphasized/60",
+                }}
+                _selected={{
+                  bg: "bg.muted",
+                }}
+              >
+                <Box />
+
+                <TreeView.ItemText fontSize="md">{node.name}</TreeView.ItemText>
+              </TreeView.Item>
+            )
+          }
+        />
+      </TreeView.Tree>
+    </TreeView.Root>
+  );
+}
